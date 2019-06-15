87bd342b5ae9f7ce84d6d737d5482d3fd6805f0f
h4ckfu <h4ckfu@protonmail.com>
2019-06-15 14:08:11 -0400

    added jobs and blog

--- xtranet/settings.py
+++ xtranet/settings.py
@@ -75,11 +75,15 @@ WSGI_APPLICATION = 'xtranet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'xtranetdb',
+        'USER': 'postgres',
+        'PASSWORD': 'pUpod!fred#',
+        'HOST': 'localhost',
+        'PORT': '5432'
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
